e2368fd508a8e85e6737a3f879360ae802330b6f
priyakhokher <pnk230@nyu.edu>
2016-05-25 21:27:31 -0700

    comments

--- clusterSongs1.py
+++ clusterSongs1.py
@@ -6,15 +6,18 @@ albums_path = "C:\Users\priya\OneDrive\Documents\Coldplay\Albums/"
 albums = os.listdir(albums_path)
 #You have songs from 7 albums
 #['AHeadFullOfDreams', 'ARushOfBloodToTheHead', 'GhostStories', 'MyloXyloto', 'Parachutes', 'VivaLaVida', 'X&Y']
+#In Total there are 73 songs
 songLyrics = pd.DataFrame(columns = ['songs','lyrics'], index = range(73))
 songLyrics = songLyrics.fillna(0)
 t = 0
 songsL = []
 for album in albums:
+	#go into each album
 	print album
 	path = albums_path+'/'+album + "/"
+	# grab each each song
 	for i in os.listdir(path):
-		
+		#store the lyrics and song
 		print t
 		print i
 		with open(path+i) as songs:
